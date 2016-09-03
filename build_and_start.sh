2c07941acc670d33eada459ad43e0b1d110f5c3d
Roselma Mendes <roselma.mendes@gmail.com>
2016-09-03 14:00:19 -0300

    Update the name of the file in the command.

--- build_and_start.sh
+++ build_and_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t security-flask .
-docker run -p 5000:5000 -t security-flask python black_girls/black_girls.py
+docker run -p 5000:5000 -t security-flask python black_girls/black_girls_app.py
