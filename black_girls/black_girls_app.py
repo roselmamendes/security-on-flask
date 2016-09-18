2c18faabdebb249fe441940febdcaf02ac6f2a84
Roselma Mendes <roselma.mendes@gmail.com>
2016-09-18 17:11:32 -0300

    Add Flask Login.

--- black_girls/black_girls_app.py
+++ black_girls/black_girls_app.py
@@ -1,27 +1,93 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+import flask_login
+import base64
+import json
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 girls = []
 
-@app.route('/girls', methods = ['GET', 'POST'])
+#Authentication
+login_manager = flask_login.LoginManager()
+
+login_manager.init_app(app)
+
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(girl_id):
+    return girl_by_id(girl_id)
+
+def girl_by_id(girl_id):
+    for girl in girls:
+        if girl.id == girl_id:
+            return girl
+    return None
+
+@login_manager.request_loader
+def request_loader(request):
+    token = request.headers.get('Authorization')
+    token = token.replace('Basic', '', 1) if token else ''
+    id = decode_token(token)
+    return girl_by_id(id)
+
+def decode_token(token):
+    return base64.b64decode(token).decode("utf-8")
+
+@app.route('/girls/<string:id>', methods = ['GET'])
+@flask_login.login_required
+def get_girl(id):
+    json_girl = mixin_to_json(girl_by_id(id))
+    return jsonify(json_girl)
+
+@app.route('/girls', methods = ['GET'])
 def all_girls():
     resp = 'Welcome'
-
-    if request.method == 'GET':
-        resp = jsonify(girls)
-        resp.mimetype = 'application/json'
-
-    if request.method == 'POST':
-       girls.append(request.get_json(force=True))
-       resp = jsonify(girls[-1])
-       resp.status_code = 201
-       resp.mimetype = 'application/json'
-
+    json_girls = mixin_list_to_json_list(girls)
+    resp = jsonify(json_girls)
+    resp.mimetype = 'application/json'
     return resp
+
+@app.route('/girls', methods = ['POST'])
+def create_girl():
+    user_json = request.get_json(force=True)
+
+    user = json_to_mixin(user_json)
+    girls.append(user)
+
+    resp = jsonify(mixin_to_json(girls[-1]))
+    resp.status_code = 201
+    resp.mimetype = 'application/json'
+    return resp
+
+
+def json_to_mixin(user_json):
+    user = User()
+    user.id = user_json['id']
+    user.github = user_json['github']
+    user.name = user_json['name']
+    user.profissao = user_json['profissao']
+    return user
+
+
+def mixin_list_to_json_list(mixin_list):
+    json_list = []
+    for mixin in mixin_list:
+        json_list.append(mixin_to_json(mixin))
+
+    return json_list
+
+def mixin_to_json(usermixin):
+    return {
+        "id": usermixin.id,
+        "name": usermixin.name,
+        "github": usermixin.github,
+        "profissao": usermixin.profissao
+    }
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- black_girls/tests/test_black_girls_app.py
+++ black_girls/tests/test_black_girls_app.py
@@ -1,6 +1,8 @@
 import unittest
 import json
 from black_girls import black_girls_app
+import uuid
+import base64
 
 class TestBlackGirl(unittest.TestCase):
 
@@ -12,7 +14,8 @@ class TestBlackGirl(unittest.TestCase):
         black_girls_app.girls = []
 
     def test_should_return_all_saved_girls(self):
-        data = json.dumps(dict(name='Nina', profissao='dev', github='nina99'))
+        data = json.dumps(dict(id='1', name='Nina', profissao='dev', \
+                                                         github='nina99'))
         resp = self.app.post('/girls', data=data)
 
         self.assertEqual(201, resp.status_code, resp.data)
@@ -21,7 +24,7 @@ class TestBlackGirl(unittest.TestCase):
 
         actual_resp = json.loads(resp.data.decode('utf8'))
         expected_resp = [
-            {'name': 'Nina', 'profissao': 'dev', 'github': 'nina99'}
+            {'id': '1', 'name': 'Nina', 'profissao': 'dev', 'github': 'nina99'}
         ]
 
         self.assertEqual(200, resp.status_code)
@@ -29,7 +32,8 @@ class TestBlackGirl(unittest.TestCase):
         self.assertEqual(expected_resp, actual_resp)
 
     def test_should_create_a_girl_record(self):
-        expected_resp = json.dumps(dict(name='Nina', profissao='dev', github='nina99'))
+        expected_resp = json.dumps(dict(id='1', name='Nina', profissao='dev',
+                                        github='nina99'))
 
         resp = self.app.post('/girls', data=expected_resp)
 
@@ -37,3 +41,41 @@ class TestBlackGirl(unittest.TestCase):
 
         self.assertEqual(201, resp.status_code, resp.data)
         self.assertEqual(json.loads(expected_resp), actual_resp)
+
+    def test_girl_by_id_should_return_a_girl_dict(self):
+        id1 = str(uuid.uuid4())
+        id2 = str(uuid.uuid4())
+
+        girl = json.dumps(
+            dict(id=id1, name='Nina', profissao='dev', github='nina99'))
+        girl2 = json.dumps(
+            dict(id=id2, name='Roberta', profissao='tester', github='rob99'))
+
+        resp = self.app.post('/girls', data=girl)
+        resp = self.app.post('/girls', data=girl2)
+
+        actual_girl = black_girls_app.girl_by_id(id1)
+        actual_girl2 = black_girls_app.girl_by_id(id2)
+
+        self.assertEqual('Nina', actual_girl.name)
+        self.assertEqual('Roberta', actual_girl2.name)
+
+    def test_should_return_a_girl_when_passing_a_token(self):
+        id1 = str(uuid.uuid4())
+        token = base64.b64encode(bytes(id1, 'utf-8'))
+
+        girl = json.dumps(
+            dict(id=id1, name='Nina', profissao='dev', github='nina99'))
+
+        resp = self.app.post('/girls', data=girl)
+
+        resp = self.app.get(
+            '/girls/{}'.format(id1),
+            headers={'Authorization': 'Basic {}'.format(token.decode('utf-8'))})
+
+        self.assertEqual(200, resp.status_code, resp.data)
+
+        actual_resp = json.loads(resp.data.decode('utf8'))
+        self.assertEqual(json.loads(girl), actual_resp)
+
+        self.assertEqual('Nina', actual_resp['name'])
